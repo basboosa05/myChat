cb44f3400a577e612b429409a5b975efb5f8440a
basboosa05 <166862890+basboosa05@users.noreply.github.com>
2025-03-22 00:57:01 +0200

    flask directories
    changed the files structure to suit flask working environment

--- UI/app.py
+++ UI/app.py
@@ -1,10 +1,16 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from models import db, User, Friend, Message 
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 db.init_app(app)  # Initialize db with the app
+
+@app.route("/")
+def index():
+    return render_template("index2.html")
 
 # Routes and other logic go here
 if __name__ == '__main__':
