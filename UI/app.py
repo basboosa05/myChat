9237d7fb21cb611e5bb3a68ce92289d6a1a5a8ca
basboosa05 <166862890+basboosa05@users.noreply.github.com>
2025-03-22 02:49:17 +0200

    some modifications
    in the routing and db configuration

--- UI/app.py
+++ UI/app.py
@@ -1,16 +1,20 @@
 # app.py
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session, url_for
 from models import db, User, Friend, Message 
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
+app.secret_key="your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 db.init_app(app)  # Initialize db with the app
 
 @app.route("/")
-def index():
-    return render_template("index2.html")
+def home():
+    if "username" in session:
+        return redirect(url_for('index2'))
+    return render_template("Login.html")
 
 # Routes and other logic go here
 if __name__ == '__main__':
